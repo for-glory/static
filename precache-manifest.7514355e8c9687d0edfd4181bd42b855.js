491d3479a4b6f3cd973434cd66e170552cf63ff1
For Glory <forglory09051@gmail.com>
2022-10-23 04:03:14 -0700

    fix homepage issue

---
+++ precache-manifest.7514355e8c9687d0edfd4181bd42b855.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9853ef5e6964cee1353394bf7bf345ca",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "8d6b0b4d2fdba1f2cfb8",
+    "url": "/static/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "1db59fedfb07fbdebc71",
+    "url": "/static/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "00d05936166d91b7ed49",
+    "url": "/static/static/css/main.429a64d0.chunk.css"
+  },
+  {
+    "revision": "3bc506f026d7d5df40df",
+    "url": "/static/static/js/2.af8ad87c.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/static/js/2.af8ad87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6b0b4d2fdba1f2cfb8",
+    "url": "/static/static/js/3.7abc2f69.chunk.js"
+  },
+  {
+    "revision": "1db59fedfb07fbdebc71",
+    "url": "/static/static/js/4.eb94a677.chunk.js"
+  },
+  {
+    "revision": "00d05936166d91b7ed49",
+    "url": "/static/static/js/main.93b12308.chunk.js"
+  },
+  {
+    "revision": "e16de62b08d2cee95eab",
+    "url": "/static/static/js/runtime-main.61fda120.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "7ee0adf5d5a73219d95393baaeb1d098",
+    "url": "/static/static/media/elusiveLogo1.7ee0adf5.png"
+  },
+  {
+    "revision": "705f18e5be95cb9c4bec7e999a7a6ab9",
+    "url": "/static/static/media/elusiveLogo2.705f18e5.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "83bdaa60e70e057a5def47b01f536b56",
+    "url": "/static/static/media/meprismLogo.83bdaa60.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "e10acd4135f62388f77411f1cabf6908",
+    "url": "/static/static/media/tellusLogo.e10acd41.png"
+  }
+]);
